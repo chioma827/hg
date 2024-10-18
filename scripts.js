a01e522611c0e8ba58fc23a791cdad1b0d246cdf
chioma827 <okerekechioma827@gmail.com>
2024-10-18 19:12:47 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// script.js  
+
+document.getElementById('hamburger').addEventListener('click', function() {  
+    const navLinks = document.querySelector('.nav-links');  
+    navLinks.classList.toggle('active');  
+});
